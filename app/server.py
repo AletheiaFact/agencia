33026dab7e8b0764465485b06e065bb35c1317b8
msantos <msantos@wikimedia.org>
2024-09-17 14:06:20 -0300

    Stop using unfinished auth_middleware

--- app/server.py
+++ app/server.py
@@ -2,7 +2,8 @@ from graph import WorkFlow
 from fastapi import FastAPI, Request, Depends
 from langserve import add_routes
 from fastapi.responses import JSONResponse
-from middleware.auth_middleware import AuthMiddleware, verify_token
+
+# from middleware.auth_middleware import AuthMiddleware, verify_token
 from fastapi.security import OAuth2PasswordBearer
 
 agents_app = WorkFlow().app
@@ -13,16 +14,16 @@ app = FastAPI(
     description="aletheia API automatedFactChecking server using LangChain and CrewAI",
 )
 
+
 @app.post("/invoke")
 async def stream(request: Request):
     req = await request.json()
     result = agents_app.invoke(req["input"])
-    return JSONResponse(content={"message": result })
+    return JSONResponse(content={"message": result})
 
-add_routes(
-    app,
-    agents_app
-)
+
+add_routes(app, agents_app)
+
 
 # Health Check endpoint for k8s livenes probe
 @app.get("/health", tags=["Health Check"])
@@ -30,7 +31,8 @@ async def health_check():
     # Here you might add logic to verify service health, like DB connection etc.
     return {"status": "ok"}
 
-app.add_middleware(AuthMiddleware)
+
+# app.add_middleware(AuthMiddleware)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
